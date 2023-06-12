c00ec81cbaf5e512bb379e441f5bc7f0ca05cfb7
gavron22 <gavron22@gmail.com>
2023-06-12 17:20:16 +0200

    Quiz v1

---
+++ script.js
@@ -0,0 +1,101 @@
+import {zbiorPytan} from './zbiorPytan.js'
+
+const przyciskStart = document.getElementById("startButton")
+const przyciskNastepne = document.getElementById("nastepneButton")
+const ekranStartowy = document.getElementById("startContainer")
+const ekranQuizu = document.getElementById("pytaniaContainer")
+const pytanie = document.getElementById("trescPytania")
+const odpowiedzi = document.getElementById("odpowiedzi")
+przyciskStart.addEventListener("click", rozpocznijGre)
+
+// zmienne
+var wybranaOdp
+var aktualnePytanie
+var punkty = 0
+var zadanePytania = 0
+var wykorzystanePytania = []
+
+
+function rozpocznijGre() {
+    ekranStartowy.style.display = "none";
+    ekranQuizu.style.display = "block";
+    nastepnePytanie()
+
+}
+
+function nastepnePytanie() {
+    var index = Math.floor(Math.random() * zbiorPytan.length)
+    while (wykorzystanePytania.includes(index)) {
+        index = Math.floor(Math.random() * zbiorPytan.length)
+    }
+    wykorzystanePytania.push(index)
+    aktualnePytanie = zbiorPytan[index]
+    pytanie.innerText = `Pytanie ${zadanePytania+1}/25\n${aktualnePytanie.pytanie}`
+    przyciskNastepne.addEventListener("click", sprawdzOdpowiedz)
+
+    aktualnePytanie.odpowiedzi.forEach(element => {
+        var odp = document.createElement("button")
+        odp.innerText = element
+        odp.classList.add("element")
+        odp.style.fontSize = "1.4rem"
+        odp.addEventListener("click", () => {
+            wybranaOdp = odp.innerText
+            console.log(odp.innerText)
+            wybierzOdpowiedz(odp)
+        })
+        odpowiedzi.appendChild(odp)
+    });
+} 
+
+function wybierzOdpowiedz(button) {
+    var selected = button.classList.contains("selected");
+    deselectAllButtons();
+    if (!selected) {
+      button.classList.add("selected");
+      wybranaOdp = button.innerText;
+    } else {
+      wybranaOdp = null;
+    }
+    przyciskNastepne.addEventListener("click", sprawdzOdpowiedz);
+  }
+
+// function wybierzOdpowiedz(event) {
+//     wybranaOdp = event.target.innerText;
+//     deselectAllButtons();
+//     event.target.classList.add("selected");
+//     przyciskNastepne.addEventListener("click", sprawdzOdpowiedz);
+//   }
+
+function deselectAllButtons() {
+    var buttons = odpowiedzi.getElementsByClassName("element");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove("selected");
+    }
+}
+
+function sprawdzOdpowiedz() {
+    if (wybranaOdp === aktualnePytanie.poprawna) {
+        punkty += 1
+        pytanie.innerText = `Dobrze!\n Ilość punktów: ${punkty}`;
+    } else {
+        pytanie.innerText = `Źle!\n Ilość punktów: ${punkty}`
+    }
+    zadanePytania += 1
+    odpowiedzi.innerHTML = '';
+    przyciskNastepne.removeEventListener("click", sprawdzOdpowiedz)
+    wybranaOdp = null
+    if (zadanePytania >= 25) {
+        zakonczQuiz()
+    } else {
+        setTimeout(nastepnePytanie, 1200)
+    }
+}
+
+function zakonczQuiz() {
+    if (punkty >= 19) {
+        pytanie.innerText = `Gratulacje, Wygrałeś!\nIlość zdobytych punktów: ${punkty}/25`;
+    } else {
+        pytanie.innerText = `Tym razem się nie udało!\n Ilość zdobytych punktów: ${punkty}/25`;
+    }
+        odpowiedzi.innerHTML = '';
+}
---
+++ zbiorPytan.js
@@ -0,0 +1,78 @@
+export const zbiorPytan = [
+    {
+        pytanie: "Gdzie znajduje się Machu Picchu, starożytne miasto Inków",
+        odpowiedzi: ["Kolumbia", "Brazylia", "Meksyk", "Peru"],
+        poprawna: "Peru"
+    },
+    {
+        pytanie: "Jaka jest stolica Argentyny?",
+        odpowiedzi: ["Buenos Aires", "Santiago", "Sao Paulo", "Lima"],
+        poprawna: "Buenos Aires"
+    },
+    {
+        pytanie: "Które z tych miast leży na dwóch kontynentach?",
+        odpowiedzi: ["Stambuł", "Moskwa", "Kair", "Dubaj"],
+        poprawna: "Stambuł"
+    },
+    {
+        pytanie: "Gdzie znajduje się Gwinea Równikowa?",
+        odpowiedzi: ["Oceania", "Azja", "Ameryka Południowa", "Afryka"],
+        poprawna: "Afryka"
+    },
+    {
+        pytanie: "Które z tych państw nie graniczy z Rosją?",
+        odpowiedzi: ["Mongolia", "Norwegia", "Ukraina", "Białoruś"],
+        poprawna: "Norwegia"
+    },
+
+    {
+        pytanie: "Które z tych państw jest najmniejsze pod względem powierzchni?",
+        odpowiedzi: ["Monako", "Watykan", "Nauru", "Malta"],
+        poprawna: "Watykan"
+    },
+    {
+        pytanie: "Jaki jest największy kraj Ameryki Południowej?",
+        odpowiedzi: ["Argentyna", "Brazylia", "Kolumbia", "Peru"],
+        poprawna: "Brazylia"
+    },
+    {
+        pytanie: "Która z rzek jest najdłuższa na świecie?",
+        odpowiedzi: ["Amazonka", "Nil", "Jangcy", "Missisipi"],
+        poprawna: "Nil"
+    },
+    {
+        pytanie: "Które z tych państw nie posiada dostępu do morza?",
+        odpowiedzi: ["Boliwia", "Paragwaj", "Urugwaj", "Chile"],
+        poprawna: "Boliwia"
+    },
+    {
+        pytanie: "Jaki jest język urzędowy w Kenii?",
+        odpowiedzi: ["Angielski", "Arabski", "Francuski", "Suahili"],
+        poprawna: "Suahili"
+    },
+
+    {
+        pytanie: "Które z tych państw jest domem dla Świątyni Buddy w Kamakurze?",
+        odpowiedzi: ["Chiny", "Indie", "Japonia", "Kambodża"],
+        poprawna: "Japonia"
+    },
+
+    {
+        pytanie: "Jak nazywa się słynne sanktuarium na szczycie góry Kailash w Tybecie?",
+        odpowiedzi: ["Taj Mahal", "Akropol", "Angkor Wat", "Kailashnath"],
+        poprawna: "Kailashnath"
+    },
+        
+    {
+        pytanie: "Gdzie znajduje się Zamek Neuschwanstein, inspiracja dla Zamku Kopciuszka z Disneya?",
+        odpowiedzi: ["Niemcy", "Francja", "Włochy", "Szkocja"],
+        poprawna: "Niemcy"
+    },
+        
+    {
+        pytanie: "Które z tych miast jest znane z Fontanny di Trevi?",
+        odpowiedzi: ["Rzym, Włochy", "Barcelona, Hiszpania", "Ateny, Grecja", "Paryż, Francja"],
+        poprawna: "Rzym, Włochy"
+    },
+
+]
