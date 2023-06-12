4c22d498107660c580e74429a7f8e7fc18452e51
gavron22 <gavron22@gmail.com>
2023-06-12 17:34:42 +0200

    added more questions

--- zbiorPytan.js
+++ zbiorPytan.js
@@ -24,7 +24,6 @@ export const zbiorPytan = [
         odpowiedzi: ["Mongolia", "Norwegia", "Ukraina", "Białoruś"],
         poprawna: "Norwegia"
     },
-
     {
         pytanie: "Które z tych państw jest najmniejsze pod względem powierzchni?",
         odpowiedzi: ["Monako", "Watykan", "Nauru", "Malta"],
@@ -50,29 +49,84 @@ export const zbiorPytan = [
         odpowiedzi: ["Angielski", "Arabski", "Francuski", "Suahili"],
         poprawna: "Suahili"
     },
-
     {
         pytanie: "Które z tych państw jest domem dla Świątyni Buddy w Kamakurze?",
         odpowiedzi: ["Chiny", "Indie", "Japonia", "Kambodża"],
         poprawna: "Japonia"
     },
-
     {
         pytanie: "Jak nazywa się słynne sanktuarium na szczycie góry Kailash w Tybecie?",
         odpowiedzi: ["Taj Mahal", "Akropol", "Angkor Wat", "Kailashnath"],
         poprawna: "Kailashnath"
     },
-        
     {
         pytanie: "Gdzie znajduje się Zamek Neuschwanstein, inspiracja dla Zamku Kopciuszka z Disneya?",
         odpowiedzi: ["Niemcy", "Francja", "Włochy", "Szkocja"],
         poprawna: "Niemcy"
-    },
-        
+    },        
     {
         pytanie: "Które z tych miast jest znane z Fontanny di Trevi?",
         odpowiedzi: ["Rzym, Włochy", "Barcelona, Hiszpania", "Ateny, Grecja", "Paryż, Francja"],
         poprawna: "Rzym, Włochy"
     },
-
+    {
+        pytanie: "Jak nazywa się słynna świątynia w Kambodży, która jest również symbolem kraju?",
+        odpowiedzi: ["Borobudur", "Ta Prohm", "Angkor Wat", "Wat Arun"],
+        poprawna: "Angkor Wat"
+    },        
+    {
+        pytanie: "Gdzie znajduje się Bazylika Matki Bożej Fatimskiej?",
+        odpowiedzi: ["Portugalia", "Włochy", "Francja", "Hiszpania"],
+        poprawna: "Portugalia"
+    },        
+    {
+        pytanie: "Które z tych miast jest domem dla Bazyliki Matki Bożej z Guadalupe?",
+        odpowiedzi: ["Meksyk", "Kolumbia", "Peru", "Argentyna"],
+        poprawna: "Meksyk"
+    },        
+    {
+        pytanie: "Gdzie znajduje się Wielki Kanion Kolorado?",
+        odpowiedzi: ["Stany Zjednoczone", "Kanada", "Meksyk", "Chile"],
+        poprawna: "Stany Zjednoczone"
+    },        
+    {
+        pytanie: "Jak nazywa się słynny pomnik w Brazylii przedstawiający Chrystusa Zbawiciela?",
+        odpowiedzi: ["Pomnik Niepodległości", "Pomnik Chrystusa Odkupiciela", "Pomnik Króla Jana III", "Pomnik Patriotyzmu"],
+        poprawna: "Pomnik Chrystusa Odkupiciela"
+    },
+    {
+        pytanie: "Które z tych miast jest znane jako stolica kina?",
+        odpowiedzi: ["Nowy Jork, Stany Zjednoczone", "Londyn, Wielka Brytania", "Paryż, Francja", "Los Angeles, Stany Zjednoczone"],
+        poprawna: "Los Angeles, Stany Zjednoczone"
+    },        
+    {
+        pytanie: "Gdzie znajduje się Broadway, słynna dzielnica teatrów i musicali?",
+        odpowiedzi: ["Nowy Jork, Stany Zjednoczone", "Londyn, Wielka Brytania", "Paryż, Francja", "Berlin, Niemcy"],
+        poprawna: "Nowy Jork, Stany Zjednoczone"
+    },
+    {
+        pytanie: "Które z tych miast jest znane jako mekka muzyki country?",
+        odpowiedzi: ["Nashville, Stany Zjednoczone", "Austin, Stany Zjednoczone", "Memphis, Stany Zjednoczone", "Detroit, Stany Zjednoczone"],
+        poprawna: "Nashville, Stany Zjednoczone"
+    },        
+    {
+        pytanie: "Gdzie znajduje się Abbey Road, słynne przejście dla pieszych z okładki albumu zespołu The Beatles?",
+        odpowiedzi: ["Londyn, Wielka Brytania", "Nowy Jork, Stany Zjednoczone", "Liverpool, Wielka Brytania", "Los Angeles, Stany Zjednoczone"],
+        poprawna: "Londyn, Wielka Brytania"
+    },        
+    {
+        pytanie: "Które z tych miast jest domem dla słynnego festiwalu filmowego o nazwie 'Cannes'?",
+        odpowiedzi: ["Paryż, Francja", "Berlin, Niemcy", "Cannes, Francja", "Toronto, Kanada"],
+        poprawna: "Cannes, Francja"
+    },        
+    {
+        pytanie: "Gdzie znajduje się Stadion Olimpijski, który był miejscem rozgrywania Letnich Igrzysk Olimpijskich w 2008 roku?",
+        odpowiedzi: ["Pekin, Chiny", "Sydney, Australia", "Ateny, Grecja", "Rzym, Włochy"],
+        poprawna: "Pekin, Chiny"
+    },        
+    {
+        pytanie: "Które z tych miast jest uważane za kolebkę jazzu?",
+        odpowiedzi: ["Nowy Orlean, Stany Zjednoczone", "Chicago, Stany Zjednoczone", "Nowy Jork, Stany Zjednoczone", "San Francisco, Stany Zjednoczone"],
+        poprawna: "Nowy Orlean, Stany Zjednoczone"
+    }
 ]
